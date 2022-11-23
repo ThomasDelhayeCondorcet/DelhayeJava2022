2d4519785eb775fd27ed5f6c9fbff886b76f0417
Thomas DELHAYE <thomas.delhaye@condorcet.be>
2022-11-23 15:33:23 +0100

    PlayerDao : Adding the code for the creation of a player account (create) UserDao : Adding the code for log a user(find(pseudo, password))

--- src/be/TD/Dao/PlayerDAO.java
+++ src/be/TD/Dao/PlayerDAO.java
@@ -1,6 +1,8 @@
 package be.TD.Dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import be.TD.POJO.Player;
 
@@ -13,8 +15,35 @@ public class PlayerDAO extends DAO<Player> {
 
 	@Override
 	public boolean create(Player obj) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		boolean success = false;
+		
+		String query = "Insert into User (UserName,Pseudo,Email,Password,BirthDay,Adress,CreationDate,Balance,Moderator) Values (?,?,?,?,?,?,?,?,?)";
+		
+		try {
+			PreparedStatement stmt = this.connect.prepareStatement(query);
+			
+			stmt.setString(1, obj.getUsername());
+			stmt.setString(2, obj.getPseudo());
+			stmt.setString(3, obj.getEmail());
+			stmt.setString(4, obj.getPassword());
+			stmt.setObject(5, obj.getDateOfBirth());
+			stmt.setString(6, obj.getAdress());
+			stmt.setObject(7, obj.getRegistrationDate());
+			stmt.setInt(8, 10);
+			stmt.setInt(9, 0);
+			
+			stmt.execute();
+			stmt.close();
+			success = true;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return success;
+		
+	
 	}
 
 	@Override
--- src/be/TD/Dao/UserDAO.java
+++ src/be/TD/Dao/UserDAO.java
@@ -1,7 +1,12 @@
 package be.TD.Dao;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
 
+import be.TD.POJO.Administrator;
+import be.TD.POJO.Player;
 import be.TD.POJO.User;
 
 public class UserDAO extends DAO<User> {
@@ -33,5 +38,32 @@ public class UserDAO extends DAO<User> {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	public User find(String pseudo, String password) {
+		User user = null;
+		String query ="Select * From User Where Pseudo= and "+ pseudo + " and Password = " + password;
+		try{
+			ResultSet result = this.connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY).executeQuery(query);
+			if(result.first()) {
+				String username = result.getString("UserName");
+				String email = result.getString("Email");
+				String adress = result.getString("Adress");
+				
+				LocalDate registrationDate = result.getDate("CreationDate").toLocalDate();
+				LocalDate birthDay = result.getDate("BirthDay").toLocalDate();
+				
+				int balance = result.getInt("Balance");
+				int moderator = result.getInt("Moderator");
+				int id = result.getInt("Id");
+				if(moderator == 0)
+					return user = new Player(id, username, password, balance, pseudo, registrationDate, birthDay, email, adress );
+				if(moderator == 1)
+					return user= new Administrator(id, username, password);	
+			}
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
